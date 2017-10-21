ba4920d6745105bdff56d81cab2ed17fddf31901
syruii <stevevytran@gmail.com>
2017-10-22 02:21:14 +1100

    scheduling but incorrect hourly update

--- Lit/Lit/main.c
+++ Lit/Lit/main.c
@@ -31,11 +31,14 @@ typedef struct {
 #define SCHEDULE_0		  0x01        // ADDR for first schedule
 #define SCHEDULE_1        0x48        // ADDR for second schedule
 #define SCHEDULE_2        0x94        // ADDR for third schedule
-#define RED_OFFSET        0x00        // offset for red byte
-#define GREEN_OFFSET	  0x01		  // offset for green byte
-#define BLUE_OFFSET       0x02        // offset for blue byte
-#define HOUR_OFFSET       0x03		  // offset for another "hour"
+#define RED_OFFSET        0        // offset for red byte
+#define GREEN_OFFSET	  1		  // offset for green byte
+#define BLUE_OFFSET       2        // offset for blue byte
+#define BYTE_OFFSET	      1	      // offset for one byte	
+#define HOUR_OFFSET       0		  // offset for another "hour"
 
+
+#define BUTTON_STATE ((PINA & (1 << PA5)) >> PA5)   
 
 void update_pwm(struct rtc_time* rtc) {
 	/* Interrupt code goes here */
@@ -67,79 +70,146 @@ void update_pwm(struct rtc_time* rtc) {
 			newRGB.blue = (uint8_t) (eeprom_read_byte((uint8_t*) (SCHEDULE_2 + hour_offset + BLUE_OFFSET)));
 			break;
 	}
-	// OC0A = RED	OC1A = BLUE 	OC1B = GREEN	OC2A = WHITE
 	SColorRGB oldRGB;
-	oldRGB.red = OCR1AL;
-	oldRGB.green = OCR1BL;
+	oldRGB.red = OCR1A;
+	oldRGB.green = OCR1B;
 	oldRGB.blue = OCR0;
 	
-	int8_t red_step = (newRGB.red - oldRGB.red) /10 ;	// truncating is fine
-	int8_t green_step = (newRGB.green - oldRGB.green) /10;
-	int8_t blue_step = (newRGB.blue - oldRGB.blue) /10;
+	
+	int8_t red_step = (newRGB.red - oldRGB.red) /30 ;	// truncating is fine
+	int8_t green_step = (newRGB.green - oldRGB.green) /30;
+	int8_t blue_step = (newRGB.blue - oldRGB.blue) /30;
 	
 	// steps through 9 different color steps until reach final RGB
-	for (int i = 0; i < 9; ++i) {
-		OCR1AL += red_step;
+	for (int i = 0; i < 30; ++i) {
+		OCR1A += red_step;
 		OCR0 += blue_step;
-		OCR1BL += green_step;
-		_delay_ms(10);
+		OCR1B += green_step;
+		_delay_ms(100);
 	}
-	OCR1AL = newRGB.red;
+	OCR1A = newRGB.red;
 	OCR0 = newRGB.blue;
-	OCR1BL - newRGB.green;
+	OCR1B = newRGB.green;
 }
 // Should only be called once to program before. Date-time is set at an arbitrary date for demonstration
 // purposes default is 9:30AM in 24HOUR
 // PD4 - RESET, PD6 - CLK, PD7 - IO
 void init(struct rtc_time* rtc) {
-	uint8_t color_array[72] = { 0x00, 0xFF, 0x00,
-								0xEB, 0x78, 0xE6,
-								0x25, 0x11, 0xAE,
-								0x2D, 0x15, 0x18,
-								0x6A, 0x4B, 0xC9,
-								0x47, 0xBB, 0xCE,
-								0x00, 0xFF, 0x00,
-								0xEB, 0x78, 0xE6,
-								0x25, 0x11, 0xAE,
-								0x2D, 0x15, 0x18,
-								0x6A, 0x4B, 0xC9,
-								0x47, 0xBB, 0xCE,
-								0x00, 0xFF, 0x00,
-								0xEB, 0x78, 0xE6,
-								0x25, 0x11, 0xAE,
-								0x2D, 0x15, 0x18,
-								0x6A, 0x4B, 0xC9,
-								0x47, 0xBB, 0xCE,
-								0x00, 0xFF, 0x00,
-								0xEB, 0x78, 0xE6,
-								0x25, 0x11, 0xAE,
-								0x2D, 0x15, 0x18,
-								0x6A, 0x4B, 0xC9,
-								0x47, 0xBB, 0xCE };
+		uint8_t color_array[72] = { 0x00, 0xFF, 0x00,
+									0x00, 0x00, 0xFF,
+									0xFF, 0x00, 0x00,
+									0x2D, 0x15, 0x18,
+									0x6A, 0x4B, 0xC9,
+									0x47, 0xBB, 0xCE,
+									0x00, 0xFF, 0x00,
+									0xEB, 0x78, 0xE6,
+									0x25, 0x11, 0xAE,
+									0x2D, 0x15, 0x18,
+									0x6A, 0x4B, 0xC9,
+									0x47, 0xBB, 0xCE,
+									0x00, 0xFF, 0x00,
+									0xEB, 0x78, 0xE6,
+									0x25, 0x11, 0xAE,
+									0x2D, 0x15, 0x18,
+									0x6A, 0x4B, 0xC9,
+									0x47, 0xBB, 0xCE,
+									0x00, 0xFF, 0x00,
+									0xEB, 0x78, 0xE6,
+									0x25, 0x11, 0xAE,
+									0x2D, 0x15, 0x18,
+									0x6A, 0x4B, 0xC9,
+									0x47, 0xBB, 0xCE };
+	 uint8_t color_array2[72] = { 0x00, 0xFF, 0xFF,
+								 0xFF, 0x00, 0xFF,
+								 0xFF, 0xFF, 0x00,
+								 0xED, 0x4A, 0x4A,
+								 0x6A, 0x6A, 0xEF,
+								 0x00, 0xE3, 0x23,
+								 0x00, 0xFF, 0xFF,
+								 0xFF, 0x00, 0xFF,
+								 0xFF, 0xFF, 0x00,
+								 0xED, 0x4A, 0x4A,
+								 0x6A, 0x6A, 0xEF,
+								 0x00, 0xE3, 0x23,
+								 0x00, 0xFF, 0xFF,
+								 0xFF, 0x00, 0xFF,
+								 0xFF, 0xFF, 0x00,
+								 0xED, 0x4A, 0x4A,
+								 0x6A, 0x6A, 0xEF,
+								 0x00, 0xE3, 0x23,
+								 0x00, 0xFF, 0xFF,
+								 0xFF, 0x00, 0xFF,
+								 0xFF, 0xFF, 0x00,
+								 0xED, 0x4A, 0x4A,
+								 0x6A, 0x6A, 0xEF,
+								 0x00, 0xE3, 0x23 };
+	uint8_t color_array3[72] = { 0xC2, 0x1A, 0x11,
+								 0xE7, 0x88, 0x32,
+								 0xF2, 0xDA, 0x4A,
+								 0x90, 0xD0, 0x46,
+								 0x53, 0xBE, 0xD4,
+								 0xA2, 0x80, 0xF2,
+								 0xC2, 0x1A, 0x11,
+								 0xE7, 0x88, 0x32,
+								 0xF2, 0xDA, 0x4A,
+								 0x90, 0xD0, 0x46,
+								 0x53, 0xBE, 0xD4,
+								 0xA2, 0x80, 0xF2,
+								 0xC2, 0x1A, 0x11,
+								 0xE7, 0x88, 0x32,
+								 0xF2, 0xDA, 0x4A,
+								 0x90, 0xD0, 0x46,
+								 0x53, 0xBE, 0xD4,
+								 0xA2, 0x80, 0xF2,
+								 0xC2, 0x1A, 0x11,
+								 0xE7, 0x88, 0x32,
+								 0xF2, 0xDA, 0x4A,
+								 0x90, 0xD0, 0x46,
+								 0x53, 0xBE, 0xD4,
+								 0xA2, 0x80, 0xF2 };
 	// SET EVERYTHING AS INPUT AND ENABLE PULL UP
 	// DS1302 reconfigures later
-	DDRC = 0;
-	PORTC = 0xFF;
-	DDRB = 0;
-	PORTB = 0xFF;
-	DDRD = 0;
-	PORTD = 0XFF;
-	
+	//DDRC = 0;
+	//PORTC = 0xFF;
+	//DDRB = 0;
+	//PORTB = 0xFF;
+	//DDRD = 0;
+	//PORTD = 0XFF;
+	DDRA |= (1 << PA2) | (1 << PA0) | (1 << PA4);
+	DDRA &= ~(1 << PA5);
+	PORTA &= ~(1 << PA2);
+	PORTA &= ~(1 << PA0);
+	PORTA &= ~(1 << PA4);
 		
 	// actual code will run per hour, but for demonstration purposes, only 6
 	// schedule number is set to 0
 	eeprom_write_byte((uint8_t*)SCHEDULE_NUM, 0x00);
 	for (int i = 0; i < 72; ++i) {
-		uint8_t offset = i * RED_OFFSET;
+		uint8_t offset = i * BYTE_OFFSET;
 		eeprom_write_byte((uint8_t*)(SCHEDULE_0 + offset), color_array[i]);
 	}
-	DDRB |= (1 << PB3);    // turn on PB3 (OC2)
-	DDRD |= (1 << PD5);    // turn on PD5 (OC1A) and PD4 (OC1B)
-	DDRD |= (1 << PD4); 
-	TCCR0 |= (1 << WGM00) | (1 << WGM01) | (1 << COM01) | (1 << CS00); // fast non-inverted pwm
-	TCCR1A |= (1 << WGM10) | (1 << COM1A1) | (1 << COM1B1); // fast non-inverted pwm
+
+	for (int i = 0; i < 72; ++i) {
+		uint8_t offset = i * BYTE_OFFSET;
+		eeprom_write_byte((uint8_t*)(SCHEDULE_1 + offset), color_array2[i]);
+	}
+	
+	for (int i = 0; i < 72; ++i) {
+		uint8_t offset = i * BYTE_OFFSET;
+		eeprom_write_byte((uint8_t*)(SCHEDULE_2 + offset), color_array3[i]);
+	}
+	TCCR0 |= (1 << WGM00) | (1 << WGM01) | (1 << COM01) | (1 << CS00) | (1 << COM00); // fast non-inverted pwm
+	TCCR1A |= (1 << WGM10) | (1 << COM1A1) | (1 << COM1B1) | (1 << COM1B0) | (1 << COM1A0); // fast non-inverted pwm
 	TCCR1B |= (1 << WGM12) | ( 1 << CS10);
+	DDRB |= (1 << PB3);    // turn on PB3 (OC0)
+	DDRD |= (1 << PD5);    // turn on PD5 (OC1A) and PD4 (OC1B)
+	DDRD |= (1 << PD4);
 	// TCCR2 |= (1 << WGM21) | (1 << WGM20) | (1 << COM21) | (1 << CS20); // fast non-inverted pwm
+
+	OCR1A = 0;
+	OCR1B = 0;
+	OCR0 = 0;
+	
 	
 	ds1302_init();
 	ds1302_update(rtc);
@@ -154,11 +224,20 @@ void init(struct rtc_time* rtc) {
 	
 }
 
+void swap_schedule(void) 
+{
+	uint8_t curr_schedule = (uint8_t) (eeprom_read_byte((uint8_t*) (SCHEDULE_NUM)));
+	curr_schedule++;
+	if (curr_schedule > 2) curr_schedule = 0;
+	eeprom_write_byte((uint8_t*)SCHEDULE_NUM, curr_schedule);
+}
 
+ 
 int main(void)
 {
 	struct rtc_time ds1302;
 	struct rtc_time* rtc = &ds1302;
+	rtc->minute = 0;
 	init(rtc);
 	/*	Things to do:
 		Which sets stack variables of RGBW by reading from the EEPROM which the PWMs use
@@ -166,15 +245,35 @@ int main(void)
 		Set up PWMS on timers, where duty cycle is color-value/255%	
 		https://protostack.com.au/2011/06/atmega168a-pulse-width-modulation-pwm/
 	*/
-	
     /* Arbitrary loop */
+	unsigned char temp = rtc->minute;
+	uint8_t debounce_counter = 0;
     while (1) {
-		unsigned char temp = rtc->minute;
 		ds1302_update_time(rtc, MIN);
+		if (rtc->minute == 1) {
+			PORTA |= (1 << PA2);
+		} else if (rtc->minute == 0) {
+			PORTA |= (1 << PA0);
+		}
+		if (temp == 0) {
+			PORTA |= (1 << PA4);
+		}
 		if (temp != rtc->minute) {
 			update_pwm(rtc);
+			temp = rtc->minute;
 		}
-		
+		/*
+		if (BUTTON_STATE != 0) {
+			debounce_counter++;
+			if (debounce_counter >= 4) {
+				swap_schedule();
+				update_pwm(rtc);
+				debounce_counter = 0;
+			}
+		} else {
+			debounce_counter = 0;
+		}
+		*/
 	}
 	return 0;
 }
